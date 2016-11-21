3355029bb77572ed1969619e730709bab4286c30
Tyler <tcameronsmith@gmail.com>
2016-11-21 17:35:27 -0500

    add functions for eating 10, 100 and all dots, display options go away after dots left goes below their limits

--- pacman.js
+++ pacman.js
@@ -2,6 +2,7 @@
 var score = 0;
 var lives = 2;
 var powerPellets = 4;
+var dots = 240;
 
 // Define your ghosts here
 var inky = {
@@ -35,53 +36,91 @@ var clyde = {
 	character: 'Pokey',
 	edible: false
 };
-// replace this comment with your four ghosts setup as objects
+
 var ghosts = [inky, blinky, pinky, clyde]
 
-// Draw the screen functionality
 function drawScreen() {
 	clearScreen();
 	setTimeout(function() {
 		displayStats();
+		displayDots();
 		displayPowerPellets();
 		displayMenu();
 		displayPrompt();
 	}, 10);
-}
+};
 
 function clearScreen() {
 	console.log('\x1Bc');
-}
+};
 
 function displayStats() {
 	console.log('Score: ' + score + '     Lives: ' + lives);
-}
+};
+
+function displayDots() {
+	console.log('\nDots Remaining: ' + dots);
+};
 
 function displayPowerPellets() {
 	console.log('\nPower-Pellets: ' + powerPellets);
-}
+};
 
 function displayMenu() {
 	console.log('\nSelect Option:\n');  // each \n creates a new line
 	console.log('(d) Eat Dot');
+	if (dots > 10) {
+		console.log('(t) Eat 10 Dots');
+	};
+	if (dots > 100) {
+		console.log('(h) Eat 100 Dots');
+	};
+	if (dots > 0) {
+		console.log('(a) Eat all remaining Dots');
+	};
 	console.log('(p) Eat Power-Pellet');
 	console.log('(1) Eat Inky ' + '(' + edibleInedible(ghosts[0]) + ')');
 	console.log('(2) Eat Blinky ' + '(' + edibleInedible(ghosts[1]) + ')');
 	console.log('(3) Eat Pinky ' + '(' + edibleInedible(ghosts[2]) + ')');
 	console.log('(4) Eat Clyde ' + '(' + edibleInedible(ghosts[3]) + ')');
 	console.log('(q) Quit');
-}
+};
 
 function displayPrompt() {
 	// process.stdout.write is similar to console.log except it doesn't add a new line after the text
 	process.stdout.write('\nWaka Waka :v '); // :v is the Pac-Man emoji.
-}
+};
 
 // Menu Options
 function eatDot() {
 	console.log('\nChomp!');
 	score += 10;
-}
+	dots--;
+};
+
+function eatTenDots() {
+	if (dots > 10) {
+		console.log('\nChomp!');
+		score += 100;
+		dots -= 10;
+	};
+};
+
+function eatHundredDots() {
+	if (dots > 100) {
+		console.log('\nChomp!');
+		score += 1000;
+		dots -= 100;
+	};
+};
+
+function eatRemainingDots() {
+	if (dots > 0) {
+		console.log('\nChomp!');
+		score += (dots * 10);
+		dots -= dots;
+	};
+};
 
 function eatGhost(ghost) {
 	if (ghost.edible === false) {
@@ -91,8 +130,8 @@ function eatGhost(ghost) {
 	score += 200;
 	console.log('\n ' + ghost.name + ' has just been eaten!');
 	ghost.edible = false;
-}
-}
+};
+};
 
 function eatPowerPellet() {
 	if (powerPellets > 0) {
@@ -100,23 +139,23 @@ function eatPowerPellet() {
 		powerPellets--;
 		ghostsEdible();
 	} else {
-		console.log('\nThere are no more Power-Pellets.')
-	}
-}
+		console.log('\nThere are no more Power-Pellets.');
+	};
+};
 
 function ghostsEdible() {
 	for (var i = 0; i < ghosts.length; i++) {
 		ghosts[i].edible = true;
-	}
-}
+	};
+};
 
 function edibleInedible(ghost) {
 	if (ghost.edible === true) {
-		return "edible"
+		return "edible";
 	} else {
-		return "inedible"
-	}
-}
+		return "inedible";
+	};
+};
 // Process Player's Input
 function processInput(key) {
 	switch(key) {
@@ -126,6 +165,15 @@ function processInput(key) {
 			break;
 		case 'd':
 			eatDot();
+			break;
+		case 't':
+			eatTenDots();
+			break;
+		case 'h':
+			eatHundredDots();
+			break;
+		case 'a':
+			eatRemainingDots();
 			break;
 		case 'p':
 			eatPowerPellet();
@@ -148,13 +196,13 @@ function processInput(key) {
 			break;
 		default:
 			console.log('\nInvalid Command!');
-	}
-}
+	};
+};
 function livesCheck(lives) {
 	if (lives < 0) {
 		process.exit();
-	}
-}
+	};
+};
 //
 // YOU PROBABLY DON'T WANT TO CHANGE CODE BELOW THIS LINE
 //
